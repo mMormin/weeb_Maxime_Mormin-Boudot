8bdf2a4ac30036bb089603058c967035750753ce
Hodor DESKTOP <mmboudot@gmail.com>
2025-07-09 22:48:40 +0200

    add mobile view for Login and Contact pages

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,18 +3,18 @@ import Button from "../../components/ui/Button";
 
 const Login = () => {
   return (
-    <section className="text-white py-40">
+    <section className="text-white py-40 px-10 md:px-0">
       <div className="max-w-8xl mx-auto text-center flex flex-col items-center justify-center">
         <h2 className="text-large font-extrabold tracking-wide leading-20 mb-10">
           Connexion
         </h2>
 
-        <div className="grid grid-cols-2  bg-[#21223F] rounded-2xl p-15 gap-20">
-          <div className="w-[400px]">
+        <div className="md:grid md:grid-cols-2 bg-[#21223F] rounded-2xl p-15 md:gap-20">
+          <div className="md:w-[400px]">
             <LoginForm />
           </div>
 
-          <div className="flex-grow flex justify-center items-center">
+          <div className="flex-grow md:flex justify-center items-center hidden">
             <Button type="submit" text="Créer un compte" />
           </div>
         </div>
